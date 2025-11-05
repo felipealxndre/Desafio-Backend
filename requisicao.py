ade460c81056c57f9e77dd3ae0ff89bc12a40598
felipealxndre <161239599+felipealxndre@users.noreply.github.com>
2025-11-05 11:34:38 -0300

    Dia 2: Traduzindo Atributos

--- requisicao.py
+++ requisicao.py
@@ -1,6 +1,16 @@
 import requests
 import json
+from googletrans import Translator
 
+
+def traduzir(texto):
+    # Verifica se o texto é válido antes de traduzir
+    if not texto or texto.strip() == '':
+        return texto  # Retorna o texto original se for vazio ou None
+    
+    translator = Translator()
+    traducao = translator.translate(texto, dest='pt')
+    return traducao.text
 
 def personagens():
     url = 'https://last-airbender-api.fly.dev/api/v1/characters'
@@ -8,12 +18,23 @@ def personagens():
     # resquisição geral com parâmetros de paginação e filtro por nome
     params = {
         'page': 1,
-        'perPage': 20,
+        'perPage': 10,
         #'name': 'Aang'  # exemplo de filtro por nome
     }
 
-    resultado = requests.get(url=url, params=params)
+    personagens = requests.get(url=url, params=params).json()
 
-    print(json.dumps(resultado.json(), indent=4))
+    # traduzindo atributos específicos - name e affiliation
+    for personagem in personagens:
+
+        name = personagem.get('name', '')
+        affiliation = personagem.get('affiliation', '')
+        if name:
+            personagem['nome'] = traduzir(name)
+        if affiliation:
+            personagem['afiliacao'] = traduzir(affiliation)
+
+    print(json.dumps(personagens, indent=4, ensure_ascii=False))
+
 
 personagens()
